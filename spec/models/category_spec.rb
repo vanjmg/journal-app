154eb139bc51f0e0bf2e4cb26b8ec6fcd26604e0
vanjmg <engr.vanjmg@gmail.com>
2021-11-24 00:10:19 +0800

    Adds model spec for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can create category' do
+    Category.create(name: "Test Name")
+    expect(Category.count).to eq 1
+    end
+  
+    it 'can show category' do
+    expect(Category.find_by_name("Test Name")).to eq(@category)
+    end
+  
+    it 'can update category' do
+    Category.update(name: "Updated Name")
+    expect(Category.find_by_name("Updated Name")).to eq(@category)
+    end
 end
