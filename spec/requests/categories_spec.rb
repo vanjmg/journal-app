421ccb1ba76641918b04c208bb17dc7044428cbc
vanjmg <engr.vanjmg@gmail.com>
2021-11-24 02:17:18 +0800

    Adds Request spec

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,9 +2,41 @@ require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
   describe "GET /categories" do
-    it "works! (now write some real specs)" do
-      get categories_index_path
+    it "works!" do
+      get categories_path
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /index" do
+    it "renders a successful response" do
+      Category.create!(name: "Test Name")
+      get categories_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /show" do
+    it "renders a successful response" do
+      category = Category.create!(name: "Test Name") 
+      get category_url(category)
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /new" do
+    it "renders a successful response" do
+      get new_category_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /edit" do
+    it "render a successful response" do
+      category = Category.create!(name: "Test Name")
+      get edit_category_url(category)
+      expect(response).to be_successful
+    end
+  end
+
 end
