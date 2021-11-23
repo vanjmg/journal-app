b0327f057551c9243d69c5e1f794a0d9d2d07c2b
vanjmg <engr.vanjmg@gmail.com>
2021-11-24 01:58:55 +0800

    Adds controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,55 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   def new
+    @category = Category.new
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+
+    @category.destroy
+    respond_to do |format|
+      format.html { redirect_to categories_url, notice: "Successfully Deleted." }
+    end
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |f|
+      if @category.save
+        f.html {redirect_to @category, notice: "Successfully created"}
+      else
+        f.html (render :new)
+      end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to @category, notice: "Successfully updated." }
+      else
+        format.html { render :edit}
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
