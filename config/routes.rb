4edc6088d27b2d0fb6c79ceb7d17ed1b00ee13c6
vanjmg <engr.vanjmg@gmail.com>
2021-11-24 00:16:28 +0800

    Adds Model and Controller for Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
