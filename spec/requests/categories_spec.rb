416d6b3d4ba3db0493c675192bf607557053fb90
vanjmg <engr.vanjmg@gmail.com>
2021-11-24 02:27:14 +0800

    Adds Request spec

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
+
+  let(:valid_attributes) {{name: "Test Name"}}
+
   describe "GET /categories" do
     it "works!" do
       get categories_path
@@ -36,6 +39,58 @@ RSpec.describe "Categories", type: :request do
       category = Category.create!(name: "Test Name")
       get edit_category_url(category)
       expect(response).to be_successful
+    end
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a new Category" do
+       expect {
+          post categories_url, params: { category: valid_attributes }
+        }.to change(Category, :count).by(1)
+      end
+
+      it "redirects to the created category" do
+        post categories_url, params: { category: valid_attributes }
+        expect(response).to redirect_to(category_url(Category.last))
+      end
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+        {name: "Test Name"}
+      }
+
+      it "updates the requested category" do
+        category = Category.create! valid_attributes
+        patch category_url(category), params: { category: new_attributes }
+        category.reload
+        {name: "Updated Name"}
+      end
+
+      it "redirects to the category" do
+        category = Category.create! valid_attributes
+        patch category_url(category), params: { category: new_attributes }
+        category.reload
+        expect(response).to redirect_to(category_url(category))
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "destroys the requested category" do
+      category = Category.create! valid_attributes
+      expect {
+        delete category_url(category)
+      }.to change(Category, :count).by(-1)
+    end
+
+    it "redirects to the categories list" do
+      category = Category.create! valid_attributes
+      delete category_url(category)
+      expect(response).to redirect_to(categories_url)
     end
   end
 
